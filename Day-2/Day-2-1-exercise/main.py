517561150b39889147e803983956a03661dba664
thientanhuynh189 <tanhtctk42@gmail.com>
2021-02-24 18:05:56 -0800

    Day 2/8

---
+++ Day-2/Day-2-1-exercise/main.py
@@ -0,0 +1,8 @@
+two_digit_number = input("Type a two digit number: ")
+
+digit_number_1 = two_digit_number[0]
+digit_number_2 = two_digit_number[1]
+
+digit_number_3 = int(digit_number_1) + int(digit_number_2)
+
+print(digit_number_3)
---
+++ Day-2/Day-2-end/main.py
@@ -0,0 +1,33 @@
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+# a = float(123)
+# print(type(a))
+
+# print(70 + float("100.5"))
+
+# print(str(70) + str(100))
+
+##
+
+# 3 + 5
+# 7 - 4
+# 3 * 2
+# 6 / 3
+# 2 ** 3
+
+# PEMDASLR
+# ()
+# **
+# * /
+# + -
+
+# print(3 * 3 + 3 / 3 - 3)
+
+score = 0
+height = 1.8
+isWinning = True
+#f-String
+print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
+
