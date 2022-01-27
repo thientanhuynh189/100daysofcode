706517efcacc5c760514dc9fd9f1fefe8becec03
thientanhuynh189 <tanhtctk42@gmail.com>
2022-01-27 03:56:53 -0700

    run selenium in chrome and firefox

--- Python/Day49/main.py
+++ Python/Day49/main.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
 
-firefox_driver_path = "/home/ttannium/Documents/geckodriver"
-driver = webdriver.Firefox(executable_path=firefox_driver_path)
-driver.get("https://www.amazon.com")
+# firefox_driver_path = "/home/ttannium/Documents/geckodriver"
+# driver = webdriver.Firefox(executable_path=firefox_driver_path)
+# driver.get("https://www.linkedin.com/jobs/search/?f_AL=true&geoId=104195383&keywords=python%20developer&location=Vietnam&sortBy=R")
 
-driver.close()
+ACCOUNT_EMAIL = os.getenv('QT_IM_MODULE')
+print(ACCOUNT_EMAIL)
+
+# driver.close()
