0a6de89bb26efc628121ff613b972b1f69c52931
thientanhuynh189 <tanhtctk42@gmail.com>
2022-01-27 04:50:40 -0700

    Automation data entry in day 53

---
+++ Python/Day53/dataentryjobautomation/main.py
@@ -0,0 +1,49 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
+
+chrome_driver_path = "/home/deber/Documents/chromedriver"
+driver = webdriver.Chrome(chrome_driver_path)
+
+header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+
+response = requests.get("https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-123.279276265625%2C%22east%22%3A-121.587381734375%2C%22south%22%3A37.48708129572077%2C%22north%22%3A38.06238242908446%7D%2C%22mapZoom%22%3A9%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22sch%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D", headers=header)
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+all_link_elements = soup.select(".list-card-top a")
+print(all_link_elements)
+all_links = []
+
+for link in all_link_elements:
+    href = link["href"]
+    print(href)
+    if ("http") not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
+all_address_elements = soup.select(".list-card-info address")
+all_addresses = [address.get_text().split(" | ")[-1] for address in all_address_elements]
+
+all_price_elements = soup.select(".list-card-price")
+print(all_price_elements)
+all_prices = [price.get_text().split("+")[0] for price in all_price_elements if "$" in price.text]
+
+driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdWXZMDVVPkWTT2VGFl_dTTnUfIeK1i0j68NNd-5ajulHQ_xw/viewform")
+address = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+price = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+link = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+for n in range(len(all_links)):
+    time.sleep(2)
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
