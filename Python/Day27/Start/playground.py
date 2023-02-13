240908dbb3911b645559883cea177bd05654b3d8
thientanhuynh189 <thientanhuynh189@gmail.com>
2023-02-13 17:35:09 +0700

    Arguments and unlimited arguments

--- Python/Day27/Start/playground.py
+++ Python/Day27/Start/playground.py
@@ -2,8 +2,10 @@
 
 # Unlimited Arguments
 def add(*args):
+    sum = 0
     for n in args:
-        print(n)
+        sum += n
+    return sum
 
 # Many Positional Argument
 add(1, 2, 3, 4, 5, 6, 7)
