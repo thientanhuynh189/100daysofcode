c9516fcfd418cc9bf7910124ee5995fd9bccebf4
thientanhuynh189 <tanhtctk42@gmail.com>
2023-11-22 11:23:41 +0700

    update the title parameters of best movie

--- Python/Day45/Top100movies/main.py
+++ Python/Day45/Top100movies/main.py
@@ -9,11 +9,10 @@ website_html = response.text
 soup = BeautifulSoup(website_html, "html.parser")
 # print(soup.prettify())
 
-all_movies = soup.find_all(name="h3", class_="jsx-4245974604")
+all_movies = soup.find_all(name="h3", class_="listicleItem_listicle-item__title__hW_Kn")
 
-
-# movie_titles = [movie.getText() for movie in all_movies]
-# movies = movie_titles[::-1]
+movie_title = [movie.getText() for movie in all_movies]
+movie_title = movie_title[::-1]
 
 # with open("movies.txt", mode="w") as file:
 #     for movie in movies:
